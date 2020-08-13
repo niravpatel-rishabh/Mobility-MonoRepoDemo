77dac883530088b698b9957392b200a2137c9984
Nirav Patel <nirav.patel@turner.com>
2020-08-13 12:41:25 +0530

    - methods added

--- Commons/Sources/Commons/Commons.swift
+++ Commons/Sources/Commons/Commons.swift
@@ -1,3 +1,13 @@
-struct Commons {
+public class CommonsService {
+
+    public init() {
+
+    }
+
+    public func getStr() -> String
+    {
+        return "--Commons"
+    }
+
     var text = "Hello, World! Commons"
 }
--- Sources/Mobility/Mobility.swift
+++ Sources/Mobility/Mobility.swift
@@ -1,3 +1,18 @@
-struct Mobility {
+
+import Commons
+
+public class MobilityService {
+
+    var cms = CommonsService()
+
+    public init() {
+        
+    }
+
+    public func getHelloWorld() -> String {
+
+        return "Hello, World! Mobility with \(cms.getStr())"
+    }
+
     var text = "Hello, World! Mobility"
 }
